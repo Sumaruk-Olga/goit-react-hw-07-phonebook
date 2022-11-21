0acd51c7ed5ef5c62f7b85b8fe60cd20bbcc9158
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-11-21 18:37:08 +0200

    DebaunceInput

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,18 +2,27 @@ import { Label, Input } from "components/Filter/Filter.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { filterContacts } from "redux/contactsSlice";
 import { getFilter } from "redux/selectors";  
+import {DebounceInput} from 'react-debounce-input';
 
 export const Filter = () => {
   const dispatch = useDispatch();
   const value = useSelector(getFilter);
 
   const handleChange = (e)=>{    
-    return dispatch(filterContacts(e.currentTarget.value));
+    return dispatch(filterContacts(e.target.value));
   }
   
   return(  <Label>
     Find contacts by name
-    <Input type="text" value={value} onChange={handleChange}  title='start typing a name to search'/>
+    <DebounceInput
+          minLength={2}
+          debounceTimeout={500}
+          value={value} 
+          onChange={handleChange}          
+          title='start typing a name to search'
+          element={Input} 
+        />
+    
   </Label>
   )
 };
